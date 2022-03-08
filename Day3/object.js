dfe4e39ccec64925ac0cb9f4cfa0215bb8dd2985
amogh-gupta <amoghgupta002@gmail.com>
2022-03-08 21:51:27 +0530

    Object Decleration

---
+++ Day3/object.js
@@ -0,0 +1,13 @@
+// Object in JavaScript is just like dictionary in python and hashmap in java
+
+let obj = {};
+console.log(obj);
+
+let detail = {
+    "firstName": "Amogh",
+    "lastName": "Gupta",
+    "mobNo": 8383850328,
+    age: 24,
+}
+
+console.log(detail)
