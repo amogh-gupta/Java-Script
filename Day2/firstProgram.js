e0ed2fda4667d95e67bde0714834aa48b56eedc5
amogh-gupta <amoghgupta002@gmail.com>
2022-03-08 21:38:35 +0530

    functions

---
+++ Day2/firstProgram.js
@@ -0,0 +1,42 @@
+// To print on browser or on server
+console.log("Hey Amogh")
+
+// use "node firstProgram.js" 
+
+// Type to declare  variable: let, var, const
+
+let a = 10
+let str = "amogh"
+var char = "G"
+
+console.log(a,str,char)
+// use "node firstProgram.js" 
+// OUTPUT: 10 amogh G
+
+console.log(a+str+char)
+// use "node firstProgram.js" 
+// OUTPUT: 10amoghG
+
+for(let i=0; i<10; i++){
+    console.log(i); //semicolon(";") is ptional
+}
+
+var x = 0
+while(x<10){
+    console.log(x)
+    x++
+}
+
+function print(){
+    console.log("Hey Amogh")
+}
+print()
+
+let fun = function(){
+    console.log("Giving function reference to a variable")
+}
+fun();
+
+(function(){
+    console.log("Function are called first class citizen");
+})();
