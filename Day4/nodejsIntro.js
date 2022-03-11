f34691ac10282dd0e9103b39be4bb34e6df82367
amogh-gupta <amoghgupta002@gmail.com>
2022-03-11 08:38:31 +0530

    Child process for opening and storing other js file and execute Git commands

--- Day4/nodejsIntro.js
+++ Day4/nodejsIntro.js
@@ -3,4 +3,15 @@ let cp = require('child_process')
 // cp.execSync("calc")
 
 // For opening vscode
-cp.execSync("code")
+// cp.execSync("code")
+
+// Run the js file and print its output
+// create new js file and name it as test.js
+let content = cp.execSync("node test.js")
+console.log(content)
+console.log("output of test.js file " + content)
+
+// Using child process to execute Git commands
+console.log(""+cp.execSync("git status"))
+console.log(""+cp.execSync("git add ."))
+console.log(""+cp.execSync('git commit -m "Child process for opening and storing other js file and execute Git commands"'))
