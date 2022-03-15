46ca1ad1ca089a7f224c9a582ad5926709d30434
amogh-gupta <amoghgupta002@gmail.com>
2022-03-15 18:45:04 +0530

    Modularity -make file accessible outside the file

---
+++ Day4 - IntroToNode/testcalculator.js
@@ -0,0 +1,10 @@
+let calc = require("./calculator.js")
+console.log(calc)
+
+console.log(calc.addition(5,6))
+
+console.log(calc.subtraction(10,7))
+
+console.log(calc.division(40,8))
+
+console.log(calc.multiplication(5,4))
