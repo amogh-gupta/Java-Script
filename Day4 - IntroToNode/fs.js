cd725fdcdbeca2176f97fc1dd177ae0bb2c53071
amogh-gupta <amoghgupta002@gmail.com>
2022-03-15 16:33:54 +0530

    FS - File System, CRUD - 3) Update

--- Day4 - IntroToNode/fs.js
+++ Day4 - IntroToNode/fs.js
@@ -1,5 +1,5 @@
 let fs = require('fs');
-console.log(fs) //print features present inside the object fs
+// console.log(fs) //print features present inside the object fs
 
 let path = require("path")
 let fileName = path.join(__dirname, "file.txt")
@@ -12,8 +12,14 @@ let fileName = path.join(__dirname, "file.txt")
 // if file presented with the same name then it will overwrite the data into file.txt
 
 // 2) Read
-let content1 = fs.readFileSync(fileName, 'utf-8') //utf-8 is a encodded form(format) that we are using
-console.log(content1); // this will return the file data in user readable form (utf-8)
+// let content1 = fs.readFileSync(fileName, 'utf-8') //utf-8 is a encodded form(format) that we are using
+// console.log(content1); // this will return the file data in user readable form (utf-8)
 
-let content2 = fs.readFileSync(fileName)
-console.log(content2); // this will return the file data in buffer type
+// let content2 = fs.readFileSync(fileName)
+// console.log(content2); // this will return the file data in buffer type
+
+// 3) Update
+fs.appendFileSync(fileName,". Welcome to Java Script Tutorial") 
+// This will append the data that we are providing in the file insted of overwritting
+
+console.log(fs.readFileSync(fileName, 'utf-8')) // To read and display the content of file
