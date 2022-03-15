2e8198d0c8a6dd9477c2ff377b277876a40354f3
amogh-gupta <amoghgupta002@gmail.com>
2022-03-15 16:28:45 +0530

    FS - File System, CRUD - 2) Read

--- Day4 - IntroToNode/fs.js
+++ Day4 - IntroToNode/fs.js
@@ -7,7 +7,13 @@ let fileName = path.join(__dirname, "file.txt")
 // CRUD:- Create Read Update Delete
 
 // 1) Create
-fs.writeFileSync(fileName,"Hey, I am Amogh Gupta") 
+// fs.writeFileSync(fileName,"Hey, I am Amogh Gupta") 
 // create a file (if no file with the same name exist) with name file.txt and add the data passed as arguments
 // if file presented with the same name then it will overwrite the data into file.txt
 
+// 2) Read
+let content1 = fs.readFileSync(fileName, 'utf-8') //utf-8 is a encodded form(format) that we are using
+console.log(content1); // this will return the file data in user readable form (utf-8)
+
+let content2 = fs.readFileSync(fileName)
+console.log(content2); // this will return the file data in buffer type
