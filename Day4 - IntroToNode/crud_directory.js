ab6e7ef9815ad6e9dc93c112221d83d8564348b5
amogh-gupta <amoghgupta002@gmail.com>
2022-03-17 23:52:44 +0530

    Reading Directory content using FS module

--- Day4 - IntroToNode/crud_directory.js
+++ Day4 - IntroToNode/crud_directory.js
@@ -1,4 +1,9 @@
 let fs = require("fs")
 
 // 1) Creating directory if directory not exist
-fs.mkdirSync("hamariDirectory")
+// fs.mkdirSync("hamariDirectory") // It will create a new directory
+
+// 2) Reading directory
+let readingFilePath = __dirname
+let fileContent = fs.readdirSync(readingFilePath)
+console.log(fileContent) // It will print an array of the file present in the provided direcory path
