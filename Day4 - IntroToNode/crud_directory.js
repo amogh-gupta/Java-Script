f68131412dd9015bc3a62dd94e430b9629c7ffcb
amogh-gupta <amoghgupta002@gmail.com>
2022-03-18 00:02:19 +0530

    Deleting Directory using FS module

--- Day4 - IntroToNode/crud_directory.js
+++ Day4 - IntroToNode/crud_directory.js
@@ -1,7 +1,7 @@
 let fs = require("fs")
 
 // 1) Creating directory if directory not exist
-// fs.mkdirSync("hamariDirectory") // It will create a new directory
+fs.mkdirSync("hamariDirectory") // It will create a new directory
 
 // 2) Reading directory
 let readingFilePath = __dirname
@@ -9,4 +9,6 @@ let fileContent = fs.readdirSync(readingFilePath)
 console.log(fileContent) // It will print an array of the file present in the provided direcory path
 
 // 3) Deleting directory
-fs.rmdirSync("hamariDirectory") // It will remove the directory if the directory is empty
+// fs.rmdirSync("hamariDirectory") // It will remove the directory if the directory is empty
+
+// fs.rmdirSync("hamariDirectory", { recursive: true, force: true }) // It will remove the non empty directory
