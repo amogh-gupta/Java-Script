a2a37da00a350c35aa9a81976711b534179f8adb
amogh-gupta <amoghgupta002@gmail.com>
2022-03-18 00:05:15 +0530

    Copy file into a Directory using FS module

--- Day4 - IntroToNode/crud_directory.js
+++ Day4 - IntroToNode/crud_directory.js
@@ -12,3 +12,15 @@ console.log(fileContent) // It will print an array of the file present in the pr
 // fs.rmdirSync("hamariDirectory") // It will remove the directory if the directory is empty
 
 // fs.rmdirSync("hamariDirectory", { recursive: true, force: true }) // It will remove the non empty directory
+
+
+// 4) Copy a file
+let path = require("path")
+
+let sourcePath = path.join(__dirname,"test.js") // File path which we need to copy
+let destinationPath = path.join(__dirname, "hamariDirectory", "test.js") // final file path after copying file
+
+console.log(sourcePath)
+console.log(destinationPath)
+
+fs.copyFileSync(sourcePath, destinationPath) // copy file to the destination folder
