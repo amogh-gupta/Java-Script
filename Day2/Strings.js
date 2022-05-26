bc1ad8b5fd9afea588205b6e9bf06b6082d14579
amogh-gupta <amoghgupta002@gmail.com>
2022-05-26 23:55:54 +0530

    String Length, replace, toUpperCase, toLowerCase

--- Day2/Strings.js
+++ Day2/Strings.js
@@ -2,5 +2,14 @@ str = "I an Amogh gupta";
 console.log(str.length); // returning length of the string 
 // OUTPUT: 5
 
+// String Replace function
 let replaceStr = str.replace("an", "am")
 console.log(replaceStr);
+
+// String Upper Case Function
+let uppercase = replaceStr.toUpperCase()
+console.log(uppercase);
+
+// String Lower case function
+let lowercase = replaceStr.toLowerCase()
+console.log(lowercase);
