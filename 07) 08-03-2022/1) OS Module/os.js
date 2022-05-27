e0a8557040996ffc696805710bb5ee49c2103fc0
amogh-gupta <amoghgupta002@gmail.com>
2022-05-27 23:34:02 +0530

    Node: - OS Module

---
+++ 07) 08-03-2022/1) OS Module/os.js
@@ -0,0 +1,16 @@
+let os = require('os')
+
+// Print the OS object 
+// console.log(os)
+
+// Print System HostName
+console.log(os.hostname())
+
+// Print System Architecture
+console.log(os.arch())
+
+// Print System Network Architecture
+console.log(os.networkInterfaces())
+
+// Print System User Info
+console.log(os.userInfo())
