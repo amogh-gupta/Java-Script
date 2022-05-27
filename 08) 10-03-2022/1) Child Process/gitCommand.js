7867346e010da86a4ab5221e9ffeee3713778997
amogh-gupta <amoghgupta002@gmail.com>
2022-05-27 23:52:09 +0530

    Intro to Node: - Child Process

--- 08) 10-03-2022/1) Child Process/gitCommand.js
+++ 08) 10-03-2022/1) Child Process/gitCommand.js
@@ -1,6 +1,6 @@
 // Run this js file to update the content on Git in one click
 let cp = require("child_process")
 
-cp.execSync("git add -A")
-cp.execSync('git commit -m "Intro to Node: - Child Process"')
-cp.execSync("git push")
+console.log(cp.execSync("git add -A"))
+console.log(cp.execSync('git commit -m "Intro to Node: - Child Process"'))
+console.log(cp.execSync("git push"))
