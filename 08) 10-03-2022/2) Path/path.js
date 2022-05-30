4659f3df250d57319eb4bd5954e30c8cd13c0886
amogh-gupta <amoghgupta002@gmail.com>
2022-05-30 21:28:18 +0530

    Intro to Node: - Path - getting extension name

---
+++ 08) 10-03-2022/2) Path/path.js
@@ -0,0 +1,13 @@
+// Copy path of any file and paste it down here
+
+// F:\WebDev\JavaScript\08) 10-03-2022\2) Path\path.js
+
+let path = require("path");
+// console.log(path);
+
+
+// Getting the extension name of the file like .html, .js, .zip, .cs
+let extensionName = path.extname("F:\WebDev\JavaScript\08) 10-03-2022\2) Path\path.js");
+console.log(extensionName); 
+// OUTPUT: - .js
+
