4f67300cc8a59ea7af9273f50a628ffe8b1a0b6c
amogh-gupta <amoghgupta002@gmail.com>
2022-06-01 23:31:16 +0530

    Taking Input in JS

---
+++ 10) 15-03-2022/Question After CRUD/q1.js
@@ -0,0 +1,9 @@
+//make a folder inside same directory of this file and in that folder make a text file with content
+//new file has been made
+let fs = require("fs")
+fs.mkdirSync("q1Folder")
+
+let path = require("path")
+let filePath = path.join(__dirname,"q1Folder", "abc.txt")
+
+fs.writeFileSync(filePath, "Hello Amogh")
