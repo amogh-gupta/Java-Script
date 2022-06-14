75df00036eb0eb02bf53af9eea72082774cfed43
amogh-gupta <amoghgupta002@gmail.com>
2022-06-14 22:30:02 +0530

    How Js Works - Execution context - Eg 3

---
+++ 13) 24-03-2022/1) Execution Context/example3.js
@@ -0,0 +1,21 @@
+console.log(a)
+var a = 10
+
+var b = 20;
+console.log(b)
+
+console.log(c)
+
+function fun(){
+    return 30;
+}
+
+var c = fun()
+
+console.log(c)
+
+// OUTPUT:
+// undefined
+// 20
+// undefined
+// 30
