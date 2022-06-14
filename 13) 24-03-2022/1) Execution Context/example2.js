55eb3278922a6df020ba36f90ea2ac4a1b2718e4
amogh-gupta <amoghgupta002@gmail.com>
2022-06-14 22:26:59 +0530

    How Js Works - Execution context - Eg 2

--- 13) 24-03-2022/1) Execution Context/example2.js
+++ 13) 24-03-2022/1) Execution Context/example2.js
@@ -1,4 +1,5 @@
-fun()
+// fun() // This line will give us an error as initially fun is undefine and calling undefine() 
+// will give us an error
 
 var fun = function(){
     console.log("Hello World")
